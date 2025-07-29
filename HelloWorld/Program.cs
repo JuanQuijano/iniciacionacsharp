a35d29970bfc36b3b21dd113ffb5ee7e21908e70
Juan Quijano <listas@juanquijano.es>
2025-07-29 17:15:59 +0000

    Quito los comentarios

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,9 +1,5 @@
 ﻿Console.Clear();
 
-// Voy a modificar esta salida de texto, pero debería borrarla
-//Console.WriteLine("Esto es una cadena de texto");
-
 Console.WriteLine("¡Felicidades!");
 //Console.Write(" ");
 Console.WriteLine("¡Has creado tu primer programa en C#!");
-
