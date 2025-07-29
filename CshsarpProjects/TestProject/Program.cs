8c3c4023d04633b6c3e79c948b96d2cec28f106d
Juan Quijano <listas@juanquijano.es>
2025-07-29 19:53:03 +0000

    Actividad de tirada de dados

---
+++ CshsarpProjects/TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Random dado = new();
+var tirada1 = dado.Next();
+var tirada2 = dado.Next(101);
+var tirada3 = dado.Next(50, 101);
+var tirada4 = dado.Next(1, 7);
+
+Console.WriteLine($"Primera tirada: {tirada1}");
+Console.WriteLine($"Segunda tirada: {tirada2}");
+Console.WriteLine($"Tercera tirada: {tirada3}");
+Console.WriteLine($"Cuarta tirada: {tirada4}");
--- HelloWorld/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-
-// Este desafio es conseguir la siguiente salida
-// This is the first line.
-// This is the second line.
-
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
