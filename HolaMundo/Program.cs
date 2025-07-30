03c2d74b8a88bcedcd84eec5aff344255d6eca9f
Juan Quijano <listas@juanquijano.es>
2025-07-30 18:09:56 +0000

    Practica con string

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,13 +1,7 @@
-﻿/*Bob
-3
-34,4
-
-Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
-
-*/
-
-var nombre = "Bob";
-var numMensajes = 3;
-var temperatura = 34.4;
-
-Console.WriteLine("Hola, " + nombre + "! Tu tienes " + numMensajes + " mensajes en tu buzón de correo. La temperatura es de " + temperatura + " celsius.");
+﻿Console.Clear();
+Console.WriteLine("Hola\nMundo!");
+Console.WriteLine("Hola\tMundo!");
+Console.WriteLine("Hola \"Mundo\"!");
+Console.WriteLine(@"Hola ""Mundo""!");
+Console.WriteLine("c:\\source\\repos");
+Console.WriteLine(@"c:\source\repos");
