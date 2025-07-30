7fecce0f535ce81254858f06c223529f23fc6f6f
Juan Quijano <listas@juanquijano.es>
2025-07-30 17:19:21 +0000

    Literales y variables

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Char: " + 'b');
+Console.WriteLine("string: " + "Hola Mundo");
+Console.WriteLine("int: " + 123);
+Console.WriteLine("Float: " + 0.25F);
+Console.WriteLine("Double: " + 2.625D);
+Console.WriteLine("Decimal: " + 12.39816M);
+Console.WriteLine("Bool: " + true);
+Console.WriteLine();
+Console.WriteLine("123");
+Console.WriteLine(123);
+Console.WriteLine("True");
+Console.WriteLine(true);
+
+var saludo = "Hola Mundo";
+var saludoPorSegundaVez = "Hola Mundo por segunda vez";
+Console.WriteLine(saludo);
+Console.WriteLine(saludoPorSegundaVez);
+
+
+var ingresos = 100;
+var impuestos = 21;
+var gananciasNetas = 0f;
+
+gananciasNetas = ingresos / impuestos;
+
+Console.WriteLine(gananciasNetas);
+
