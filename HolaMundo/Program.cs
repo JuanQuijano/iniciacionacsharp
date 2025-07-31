8ed7025e2bf537e7e4d8e637dd6ce0c1cd167405
Juan Quijano <listas@juanquijano.es>
2025-07-31 17:02:01 +0000

    Incrementos y decrementos

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,23 +1,78 @@
 ﻿Console.Clear();
+
+
+var sum = 7 + 5;
+var difference = 7 - 5;
+var product = 7 * 5;
+var quotient = 7 / 5;
+// var decimalQuotient = 7.0m / 5;
+// decimalQuotient = 7 / 5.0m;
+// decimalQuotient = 7.0m / 5.0m;
+
+//int decimalQuotient = (int)(7 / 5.0m);
+//var decimalQuotient = 7.0m / 5;
+//int decimalQuotient = (int)(7.0m / 5.0m);
+//int decimalQuotient = 7 / 5;
+
+var num1 = 7;
+var num2 = 5;
+var decimalQuotient = (decimal)num1 / (decimal)num2;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
+Console.WriteLine($"Decimal quotient: {decimalQuotient}");
+
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
 /*
-View English output:
-  c:\Exercise\ACME\data.txt
-
-Посмотреть русский вывод:
-  c:\Exercise\ACME\ru-RU\data.txt
-
+Parentesis ()
+Exponentes ^
+M Multiplicación 
+D División
+A Suma (Add)
+S Resta (Substraction)
 */
 
-string projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
 
 
-var enIngles = $@"View English output:
-    c:\Exercise\{projectName}\data.txt
-";
+int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;
+value++;
 
-var enRuso = $@"{russianMessage}:
-    c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine(value);
 
-Console.WriteLine(enIngles);
-Console.WriteLine(enRuso);
+value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
+
+
+value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (value++));
