806235a586c2253e3edae9852bd942f55e98afe2
Juan Quijano <listas@juanquijano.es>
2025-07-31 19:42:51 +0000

    Ejercicio de doble y triples

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,62 +1,41 @@
 ﻿Console.Clear();
 
-/*
-Student: Sophia Johnson
+Random dice = new Random();
 
-Course          Grade   Credit Hours	
-English 101         4       3
-Algebra 101         3       3
-Biology 101         3       4
-Computer Science I  3       4
-Psychology 101      4       3
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-Final GPA:          3.35
-*/
+ /*
+ Hacer prueba de triplete
+ roll1 = 6;
+ roll2 = 6;
+ roll3 = 6;*/
 
-string studentName = "Sophia Johnson";
-string course1Name = "English 101";
-string course2Name = "Algebra 101";
-string course3Name = "Biology 101";
-string course4Name = "Computer Science I";
-string course5Name = "Psychology 101";
+int total = roll1 + roll2 + roll3;
 
-int course1Credit = 3;
-int course2Credit = 3;
-int course3Credit = 4;
-int course4Credit = 4;
-int course5Credit = 3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-int gradeA = 4;
-int gradeB = 3;
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
 
-int course1Grade = gradeA;
-int course2Grade = gradeB;
-int course3Grade = gradeB;
-int course4Grade = gradeB;
-int course5Grade = gradeA;
 
-decimal totalCreditHours = 0;
-totalCreditHours += course1Credit;
-totalCreditHours += course2Credit;
-totalCreditHours += course3Credit;
-totalCreditHours += course4Credit;
-totalCreditHours += course5Credit;
+Console.WriteLine($"Total roll: {total}");
+if (total >= 16)
+{ Console.WriteLine("You win a new car!"); }
+else if (total >= 10)
+{ Console.WriteLine("You win a new laptop!"); }
+else if (total >= 7)
+{ Console.WriteLine("You win a trip for two!"); }
+else
+{ Console.WriteLine("You win a kitten!"); }
 
-decimal totalGradePoints = 0;
-totalGradePoints += course1Credit * course1Grade;
-totalGradePoints += course2Credit * course2Grade;
-totalGradePoints += course3Credit * course3Grade;
-totalGradePoints += course4Credit * course4Grade;
-totalGradePoints += course5Credit * course5Grade;
 
-decimal gradePointAverage = totalGradePoints/totalCreditHours;
-
-Console.WriteLine($"Student: {studentName}\n");
-Console.WriteLine($"Course\t\t\tGrade\tCredit Hours");
-Console.WriteLine($"{course1Name}\t\t{course1Grade}\t{course1Credit}");
-Console.WriteLine($"{course2Name}\t\t{course2Grade}\t{course2Credit}");
-Console.WriteLine($"{course3Name}\t\t{course3Grade}\t{course3Credit}");
-Console.WriteLine($"{course4Name}\t{course4Grade}\t{course4Credit}");
-Console.WriteLine($"{course5Name}\t\t{course5Grade}\t{course5Credit}\n");
-//Console.WriteLine($"{totalGradePoints} {totalCreditHours}");
-Console.WriteLine($"Final GPA:\t\t{gradePointAverage:F2}");
