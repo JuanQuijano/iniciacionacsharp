49295303b56f6c60eec775d1d5cd6a85b7bf5872
Juan Quijano <listas@juanquijano.es>
2025-08-25 18:20:15 +0000

    Creación de código legible con convenciones, espacios en blanco y comentarios en C#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,22 +1,19 @@
-﻿Console.Clear();
-
-/*
-  The following code creates five random OrderIDs
-  to test the fraud detection process.  OrderIDs 
-  consist of a letter from A to E, and a three
-  digit number. Ex. A123.
+﻿/*
+Dada una frase, la invierte y me muestra cuantas veces aparece la letra o
 */
 
-Random random = new Random();
-string[] orderIDs = new string[5];
-for (int i = 0; i < orderIDs.Length; i++)
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+foreach (char i in charMessage) 
 {
-    int prefixValue = random.Next(65, 70);
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    string suffix = random.Next(1, 1000).ToString("000");
-    orderIDs[i] = prefix + suffix;
+    if (i == 'o')
+    { x++; } 
 }
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
-}
+
+Console.WriteLine(charMessage);
+Console.WriteLine($"'o' appears {x} times.");
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed825a8f8916c9ff8693e53b0e0a6eb785942358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0e285717ab09da7c7d8ec704d94e4819dfb10c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
