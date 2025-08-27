6d80abf2daa0d8a99bb9e6f388ef56dfbe8fece7
Juan Quijano <listas@juanquijano.es>
2025-08-27 17:28:28 +0000

    Revisión de la solución para la actividad de desafío de la instrucción switch

--- Program.cs
+++ Program.cs
@@ -1,13 +1,67 @@
 ﻿using System.Net.WebSockets;
 Console.Clear();
 
-string value = "102a";
-int result = 0;
-if (int.TryParse(value, out result))
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "03-WH-XL";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-   Console.WriteLine($"Measurement: {result}");
+   case "01":
+      type = "Sweat shirt";
+      break;
+
+   case "02":
+      type = "T-Shirt";
+      break;
+
+   case "03":
+      type = "Sweat pants";
+      break;
+
+   default:
+      type = "Other";
+      break;
 }
-else
+
+switch (product[1])
 {
-   Console.WriteLine("Unable to report the measurement.");
+   case "BL":
+      color = "Black";
+      break;
+
+   case "MN":
+      color = "Maroon";
+      break;
+
+   default:
+      color = "White";
+      break;
 }
+
+switch (product[2])
+{
+   case "S":
+      size = "Small";
+      break;
+
+   case "M":
+      size = "Medium";
+      break;
+
+   case "L":
+      size = "Large";
+      break;   
+
+   default:
+      size = "One Size Fits All";
+      break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
--- obj/Debug/net8.0/iniciacionacsharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/iniciacionacsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iniciacionacsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed2701ec769d431617a922eeecae3e71a9e46af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920cbf8bf89ebc99df9c7bf49dcad12bf4f512eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("iniciacionacsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iniciacionacsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
